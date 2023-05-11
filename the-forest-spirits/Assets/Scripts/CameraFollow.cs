614c9ac401aea1f8b22d9b0b13e7db6373680ba4
Jonathan Kula <jonathan@jdkula.dev>
2023-05-11 16:04:01 -0700

    Add movement

---
+++ the-forest-spirits/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform anchor;
+
+    [Range(0.01f, 0.3f)]
+    public float tension = 0.1f;
+    public float maxSpeed = 100;
+
+    public bool lockX = false;
+    public bool lockY = false;
+    public bool lockZ = false;
+
+    private Vector3 _velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 next = Vector3.SmoothDamp(transform.position, anchor.position, ref _velocity, tension, maxSpeed, Time.deltaTime);
+
+        if (lockX) next.x = transform.position.x;
+        if (lockY) next.y = transform.position.y;
+        if (lockZ) next.z = transform.position.z;
+
+        transform.position = next;
+    }
+}
