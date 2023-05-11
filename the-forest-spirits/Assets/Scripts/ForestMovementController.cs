61e27cbb32a4ee4e17a2711b3bd2588ff7c075e8
Jonathan Kula <jonathan@jdkula.dev>
2023-05-11 16:04:01 -0700

    Add movement

---
+++ the-forest-spirits/Assets/Scripts/ForestMovementController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ForestMovementController : MonoBehaviour
+{
+    public InputActionAsset actions;
+    public float speed = 1.0f;
+
+    private InputActionMap _map;
+    
+    private InputAction _moveAction;
+
+    private InputAction _jumpAction;
+    
+    void Awake()
+    {
+        _map = actions.FindActionMap("ForestMovement");
+        _moveAction = _map.FindAction("Move");
+        _jumpAction = _map.FindAction("Jump");
+
+        _jumpAction.performed += OnJump;
+    }
+
+    private void OnEnable()
+    {
+        _map.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _map.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = _moveAction.ReadValue<Vector2>();
+
+        transform.position += movement * (speed * Time.deltaTime);
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jumped!");
+    }
+}
