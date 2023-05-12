4cab3c78359db75516d5f328f66cfa309d299019
Jonathan Kula <jonathan@jdkula.dev>
2023-05-11 18:55:41 -0700

    Update controls, add inventory and collectable system + demo

--- the-forest-spirits/Assets/Scripts/CameraFollow.cs
+++ the-forest-spirits/Assets/Scripts/CameraFollow.cs
@@ -6,8 +6,7 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform anchor;
 
-    [Range(0.01f, 0.3f)]
-    public float tension = 0.1f;
+    [Range(0.01f, 0.3f)] public float tension = 0.1f;
     public float maxSpeed = 100;
 
     public bool lockX = false;
@@ -17,9 +16,9 @@ public class CameraFollow : MonoBehaviour
     private Vector3 _velocity;
 
     // Update is called once per frame
-    void Update()
-    {
-        Vector3 next = Vector3.SmoothDamp(transform.position, anchor.position, ref _velocity, tension, maxSpeed, Time.deltaTime);
+    void Update() {
+        Vector3 next = Vector3.SmoothDamp(transform.position, anchor.position, ref _velocity, tension, maxSpeed,
+            Time.deltaTime);
 
         if (lockX) next.x = transform.position.x;
         if (lockY) next.y = transform.position.y;
--- the-forest-spirits/Assets/Scripts/ForestMovementController.cs
+++ the-forest-spirits/Assets/Scripts/ForestMovementController.cs
@@ -7,52 +7,49 @@ using UnityEngine.InputSystem;
 public class ForestMovementController : MonoBehaviour
 {
     public InputActionAsset actions;
-    public float speed = 1.0f;
+    public float speed = 5f;
+    public float dashSpeed = 10f;
 
     private InputActionMap _map;
-    
+
     private InputAction _moveAction;
-
     private InputAction _jumpAction;
-
     private InputAction _interactAction;
-    
-    void Awake()
-    {
+    private InputAction _dashAction;
+
+    void Awake() {
         _map = actions.FindActionMap("ForestMovement");
         _moveAction = _map.FindAction("Move");
         _jumpAction = _map.FindAction("Jump");
+        _interactAction = _map.FindAction("Interact");
+        _dashAction = _map.FindAction("Dash");
 
         _jumpAction.performed += OnJump;
-
-        _interactAction = _map.FindAction("Interact");
         _interactAction.performed += OnInteract;
     }
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         _map.Enable();
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         _map.Disable();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Vector3 movement = _moveAction.ReadValue<Vector2>();
+    void Update() {
+        Vector3 movement = Vector3.right * _moveAction.ReadValue<float>();
+        bool dashing = _dashAction.IsPressed();
+        float moveSpeed = dashing ? dashSpeed : speed;
 
-        transform.position += movement * (speed * Time.deltaTime);
+        transform.position += movement * (moveSpeed * Time.deltaTime);
     }
 
-    private void OnJump(InputAction.CallbackContext context)
-    {
+    private void OnJump(InputAction.CallbackContext context) {
         Debug.Log("Jumped!");
     }
 
     private void OnInteract(InputAction.CallbackContext context) {
-        Debug.Log("Interaction");
+        Debug.Log("Interacted");
     }
 }
