7cd201bf1d73d75eaf69ba3668ff9233873c0a46
Jonathan Kula <jonathan@jdkula.dev>
2023-05-11 21:47:16 -0700

    Add floatiness!

--- the-forest-spirits/Assets/Scripts/Movement/Floaty.cs
+++ the-forest-spirits/Assets/Scripts/Movement/Floaty.cs
@@ -1,12 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Floaty : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start() { }
+    public float frequency = 4f;
+    public float magnitude = 0.1f;
 
-    // Update is called once per frame
-    void Update() { }
+    private float _t = 0f;
+    
+    private void Awake() {
+        GameObject sub = new GameObject();
+        sub.transform.parent = this.transform.parent;
+        sub.transform.localPosition = this.transform.localPosition;
+        sub.transform.localScale = this.transform.localScale;
+        sub.transform.localRotation = this.transform.localRotation;
+
+        transform.parent = sub.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localScale = Vector3.one;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    private void Update() {
+        _t += Time.deltaTime;
+
+        float delta = Mathf.Sin(2 * Mathf.PI * _t / frequency);
+        float scale = magnitude / transform.lossyScale.y;
+        
+        transform.localPosition = Vector3.up * (scale * delta);
+    }
 }
