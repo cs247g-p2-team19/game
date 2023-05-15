748d7912f4be69d2aa96a3da9a59a9bea1dc761e
Jonathan Kula <jonathan@jdkula.dev>
2023-05-14 21:35:31 -0700

    Add comments to DoorBranch.cs

--- the-forest-spirits/Assets/Scripts/Dialogue/DoorBranch.cs
+++ the-forest-spirits/Assets/Scripts/Dialogue/DoorBranch.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
 
+/** The expected state of the door for this Branch (open or closed) */
 public enum DoorState
 {
     Open,
     Closed
 }
 
+/** Represents a branch based on whether a door is open or closed */
 public class DoorBranch : Branch
 {
-    [Header("progress to")] public Branch next;
+    [Header("progress to")]
+    public Branch next;
+    
     [Header("if")]
     public Door door;
     public DoorState was;
-    [Header("otherwise")] public Branch fallbackTo;
     
-    
+    [Header("otherwise")]
+    public Branch fallbackTo;
+
+
     public override Conversation GetConversation() {
-        if (door.IsOpen == (was == DoorState.Open)) {
+        bool shouldBeOpen = was == DoorState.Open;
+        
+        if (door.IsOpen == shouldBeOpen) {
             return next.GetConversation();
         }
 
