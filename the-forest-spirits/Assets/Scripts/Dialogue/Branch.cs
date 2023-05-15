61d61a51590c0ad26ff84a7df2b0f09eab266071
Jonathan Kula <jonathan@jdkula.dev>
2023-05-14 21:35:21 -0700

    Add comments to Branch.cs

--- the-forest-spirits/Assets/Scripts/Dialogue/Branch.cs
+++ the-forest-spirits/Assets/Scripts/Dialogue/Branch.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
+/**
+ * Represents a direction that a piece of
+ * dialogue can go!
+ */
 public abstract class Branch : MonoBehaviour
 {
+    /** Returns the conversation this branch leads to! */
     public abstract Conversation GetConversation();
 }
