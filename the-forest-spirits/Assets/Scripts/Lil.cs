52b362a3681999d7c9723e9a6c10b67bbab2e517
Jonathan Kula <jonathan@jdkula.dev>
2023-05-15 03:03:48 -0700

    Clean up Lil.cs

--- the-forest-spirits/Assets/Scripts/Lil.cs
+++ the-forest-spirits/Assets/Scripts/Lil.cs
@@ -1,15 +1,12 @@
-/*
+/**
  * A lil' class with lil' shortcuts to
  * easier to access various functionality
  */
-
 public class Lil
 {
-
     public static Player Guy => Player.Instance;
     public static Player Gal => Player.Instance;
     public static Player Pal => Player.Instance;
-
 
 
     public static Inventory Inventory => Player.Instance.inventory;
