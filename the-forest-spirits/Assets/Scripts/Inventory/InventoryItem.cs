367391bfc2e6edd0b1b2e2dee067d5aa7e7a1bbf
Jonathan Kula <jonathan@jdkula.dev>
2023-05-14 21:36:19 -0700

    Rework Collectables and Inventory so that the collectables and items in the inventory are separate

--- the-forest-spirits/Assets/Scripts/Inventory/InventoryItem.cs
+++ the-forest-spirits/Assets/Scripts/Inventory/InventoryItem.cs
@@ -8,7 +8,6 @@ using Debug = System.Diagnostics.Debug;
 /// Defines an item that can be found in the inventory.
 /// </summary>
 
-[RequireComponent(typeof(Collectable))]
 public class InventoryItem : MonoBehaviour
 {
     private static Dictionary<string, InventoryItem> _items = new();
@@ -16,22 +15,33 @@ public class InventoryItem : MonoBehaviour
     public static InventoryItem GetItemById(string id) {
         return _items.TryGetValue(id, out var item) ? item : null;
     }
-
-    public Collectable Collectable { get; private set; }
-
-    public string itemId;
     
-    public bool isDroppable;
-    public bool isConsumable;
-    public UnityEvent<InventoryItem> onCollect;
-    public UnityEvent<InventoryItem> onUse;
-    public UnityEvent<InventoryItem> onDrop;
+    public string itemId;
+    public string itemName;
+    public bool startLocked = true;
+    
+    public UnityEvent<InventoryItem> onUnlock;
+    public UnityEvent<InventoryItem> onClick;
 
     private void Awake() {
-        Collectable = GetComponent<Collectable>();
         Debug.Assert(!_items.ContainsKey(itemId), "Multiple items with the same ID detected!");
         Debug.Assert(itemId != "", "Item ID may not be blank!");
 
         _items[itemId] = this;
+
+        if (startLocked) {
+            Lock();
+        }
+    }
+    
+
+    public void Unlock() {
+        gameObject.SetActive(true);
+        onUnlock.Invoke(this);
+        Lil.Guy.onUnlockItem.Invoke(this);
+    }
+
+    public void Lock() {
+        gameObject.SetActive(false);
     }
 }
