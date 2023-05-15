bae0ae9b29b3724077f0239f7e9eefdc301c3f70
Jonathan Kula <jonathan@jdkula.dev>
2023-05-15 02:33:13 -0700

    Clean up Jerky

--- the-forest-spirits/Assets/Scripts/Movement/Jerky.cs
+++ the-forest-spirits/Assets/Scripts/Movement/Jerky.cs
@@ -1,29 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+/** Alternative to Floaty: Jerky! Shakes randomly every _t seconds */
 public class Jerky : MonoBehaviour
 {
     public float frequency = 0.1f;
     public float magnitude = 0.1f;
 
-    private float _t = 0f;
-    
-    private void Awake() {
-        GameObject sub = new GameObject();
-        sub.transform.parent = this.transform.parent;
-        sub.transform.localPosition = this.transform.localPosition;
-        sub.transform.localScale = this.transform.localScale;
-        sub.transform.localRotation = this.transform.localRotation;
-
-        transform.parent = sub.transform;
-        transform.localPosition = Vector3.zero;
-        transform.localScale = Vector3.one;
-        transform.localRotation = Quaternion.identity;
-    }
+    private float _t;
 
     private void Update() {
         _t += Time.deltaTime;
@@ -33,8 +17,6 @@ public class Jerky : MonoBehaviour
 
         _t = 0;
 
-        float scale = magnitude / transform.lossyScale.y;
-        
-        transform.localPosition = Random.onUnitSphere * scale;
+        transform.localPosition = Random.onUnitSphere * magnitude;
     }
 }
