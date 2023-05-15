ebd3e582577797bfc5e2de91473a2d702f8f3013
Jonathan Kula <jonathan@jdkula.dev>
2023-05-15 02:04:43 -0700

    Clean up InventoryItem

--- the-forest-spirits/Assets/Scripts/Inventory/InventoryItem.cs
+++ the-forest-spirits/Assets/Scripts/Inventory/InventoryItem.cs
@@ -1,33 +1,46 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using Debug = System.Diagnostics.Debug;
 
-/// <summary>
-/// Defines an item that can be found in the inventory.
-/// </summary>
-
+/**
+ * Defines an item that can later be picked up.
+ * It should be attached to a GameObject in the Inventory, which
+ * will be activated when an associated Collectable is picked up
+ * (or Unlock is called on this InventoryItem).
+ */
 public class InventoryItem : MonoBehaviour
 {
-    private static Dictionary<string, InventoryItem> _items = new();
+    #region Registry
+    // Since we reference InventoryItems by their IDs, we need a registry
+    private static Dictionary<string, InventoryItem> _registry = new();
 
     public static InventoryItem GetItemById(string id) {
-        return _items.TryGetValue(id, out var item) ? item : null;
+        return _registry.TryGetValue(id, out var item) ? item : null;
     }
     
+    #endregion
+    
+    #region Unity fields
+    
+    [Tooltip("Unique item ID for this item. Can be referenced elsewhere.")]
     public string itemId;
-    public string itemName;
+    public string itemName;  // Might be unnecessary?
+    [Tooltip("Whether or not the user starts with this item. True if they start without it.")]
     public bool startLocked = true;
     
+    [Header("Events")]
     public UnityEvent<InventoryItem> onUnlock;
+    
+    [Tooltip("Invoked when this is clicked in the inventory.")]
     public UnityEvent<InventoryItem> onClick;
+    
+    #endregion
 
     private void Awake() {
-        Debug.Assert(!_items.ContainsKey(itemId), "Multiple items with the same ID detected!");
+        Debug.Assert(!_registry.ContainsKey(itemId), "Multiple items with the same ID detected!");
         Debug.Assert(itemId != "", "Item ID may not be blank!");
 
-        _items[itemId] = this;
+        _registry[itemId] = this;
 
         if (startLocked) {
             Lock();
@@ -44,4 +57,6 @@ public class InventoryItem : MonoBehaviour
     public void Lock() {
         gameObject.SetActive(false);
     }
+    
+    // TODO on click
 }
