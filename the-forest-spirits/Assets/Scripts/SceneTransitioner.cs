b98639647541110efdfec6168727767f25fb4382
Jonathan Kula <jonathan@jdkula.dev>
2023-05-16 13:20:17 -0700

    Make sure scene transitioner doesn't trigger a bunch of times

--- the-forest-spirits/Assets/Scripts/SceneTransitioner.cs
+++ the-forest-spirits/Assets/Scripts/SceneTransitioner.cs
@@ -9,13 +9,16 @@ public class SceneTransitioner : MonoBehaviour
     public static SceneTransitioner Instance => FindObjectOfType<SceneTransitioner>();
     public FadeImage imageFader;
 
+    private bool _isLoading = false;
+
     public void Load(int idx) {
+        if (_isLoading) return;
+
+        _isLoading = true;
         imageFader.FadeIn();
         Lil.Guy.FadeMusicOut();
         var ready = SceneManager.LoadSceneAsync(idx, LoadSceneMode.Single);
         ready.allowSceneActivation = false;
-        this.WaitThen(imageFader.fadeInTime + 0.1f, () => {
-            ready.allowSceneActivation = true;
-        });
+        this.WaitThen(imageFader.fadeInTime + 0.1f, () => { ready.allowSceneActivation = true; });
     }
 }
