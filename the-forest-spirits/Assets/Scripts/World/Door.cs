ef38454ab2c4f87d04706b9d56ecc5bf81b58f38
Jonathan Kula <jonathan@jdkula.dev>
2023-05-16 00:04:18 -0700

    Make sure doors only open, and enable them to transition scenes + add Forest scene and setup

--- the-forest-spirits/Assets/Scripts/World/Door.cs
+++ the-forest-spirits/Assets/Scripts/World/Door.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Animator))]
 public class Door : MonoBehaviour
 {
 
     public bool IsOpen => _animator.GetBool(Open);
+
+    public string targetSceneId;
     
     private Animator _animator;
     private static readonly int Open = Animator.StringToHash("Open");
@@ -14,7 +17,14 @@ public class Door : MonoBehaviour
     }
 
     public void DoorOpen() {
-        _animator.SetBool(Open, !IsOpen);
+        _animator.SetBool(Open, true);
     }
 
+    public void DoorClose() {
+        _animator.SetBool(Open, false);
+    }
+
+    public void TransitionScene() {
+        SceneManager.LoadScene(targetSceneId, LoadSceneMode.Single);
+    }
 }
