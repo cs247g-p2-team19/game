2c6522db4560c54e2e7629666f139faa30247867
Jonathan Kula <jonathan@jdkula.dev>
2023-05-17 16:13:29 -0700

    Basic clicking and cursor!

--- the-forest-spirits/Assets/Scripts/Utility/IClickable.cs
+++ the-forest-spirits/Assets/Scripts/Utility/IClickable.cs
@@ -3,4 +3,7 @@ using UnityEngine;
 public interface IClickable
 {
     public bool OnClick(Vector2 screenPos, Camera cam);
+
+    public bool IsClickable(Vector2 screenPos, Camera cam, out Sprite customSprite);
+
 }
