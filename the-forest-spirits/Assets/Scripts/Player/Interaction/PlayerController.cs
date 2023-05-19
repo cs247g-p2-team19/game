9d56d680fbcdf81126ded65b62d2f9d4b75e6f24
Jonathan Kula <jonathan@jdkula.dev>
2023-05-18 18:14:50 -0700

    Expose velocity on player controller so it's accessible later

--- the-forest-spirits/Assets/Scripts/Player/Interaction/PlayerController.cs
+++ the-forest-spirits/Assets/Scripts/Player/Interaction/PlayerController.cs
@@ -5,6 +5,8 @@ public abstract class PlayerController : AutoMonoBehaviour
 {
     /** True if the player shouldn't be able to move around. */
     protected bool _stopped = false;
+    
+    public abstract Vector2 Velocity { get; protected set; }
 
     public void StopMovement() {
         _stopped = true;
