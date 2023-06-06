db1bf7185a426574081233b9d207b7393ebb1da1
Jonathan Kula <jonathan@jdkula.dev>
2023-06-06 03:24:49 -0700

    Thud sound

--- the-forest-spirits/Assets/Scripts/World/Music/MusicManagerHelper.cs
+++ the-forest-spirits/Assets/Scripts/World/Music/MusicManagerHelper.cs
@@ -1,5 +1,10 @@
+using UnityEngine;
+
 public class MusicManagerHelper : AutoMonoBehaviour
 {
+    public float sfxDelay = 0f;
+    public float sfxVolume = 1f;
+
     public void Stop() {
         MusicManager.Instance.Stop();
     }
@@ -14,5 +19,14 @@ public class MusicManagerHelper : AutoMonoBehaviour
 
     public void DisableLayer(int id) {
         MusicManager.Instance.DisableLayer(id);
+    }
+
+    public void PlaySFX(AudioClip sfx) {
+        if (sfxDelay == 0f) {
+            MusicManager.Instance.PlaySFX(sfx, sfxVolume);
+        }
+        else {
+            MusicManager.Instance.WaitThen(sfxDelay, () => { MusicManager.Instance.PlaySFX(sfx, sfxVolume); });
+        }
     }
 }
