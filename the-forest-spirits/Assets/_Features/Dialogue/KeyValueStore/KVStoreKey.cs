cd575d678277c35b48450620cc457eb0c83fa9d2
Jonathan Kula <jonathan@jdkula.dev>
2023-06-06 14:00:51 -0700

    Tutorial for the first puzzle

--- the-forest-spirits/Assets/_Features/Dialogue/KeyValueStore/KVStoreKey.cs
+++ the-forest-spirits/Assets/_Features/Dialogue/KeyValueStore/KVStoreKey.cs
@@ -3,4 +3,11 @@ public enum KVStoreKey
     HasAcquiredCaseFile = 1,
     HasOpenedCaseFile = 2,
     HasSunshined = 3,
+    
+    L4HasStencil = 41,
+    L4HasNote = 42,
+    L4DidManifest = 43,
+
+    FolderOpen = 990,
+    StencilAttached = 991,
 }
