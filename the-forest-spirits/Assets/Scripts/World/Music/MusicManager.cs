14fb998e62296a45a4b16efd2eb481e0791e670c
Jonathan Kula <jonathan@jdkula.dev>
2023-06-06 02:57:45 -0700

    Fix scene transitioning + music levels

--- the-forest-spirits/Assets/Scripts/World/Music/MusicManager.cs
+++ the-forest-spirits/Assets/Scripts/World/Music/MusicManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
@@ -17,6 +18,8 @@ public class MusicManager : MonoBehaviour
 
     public float crossfade = 2f;
 
+    public float maxVolume = 0.3f;
+
     private static bool _hasInstance = false;
     private static MusicManager _instance;
 
@@ -25,7 +28,7 @@ public class MusicManager : MonoBehaviour
 
     private AudioSource _sfxSource;
     private List<AudioSource> _audios = new();
-    private MusicLayers _currentLayers;
+    private MusicLayer[] _currentLayers;
 
     private void Awake() {
         DontDestroyOnLoad(gameObject);
@@ -38,31 +41,43 @@ public class MusicManager : MonoBehaviour
         transform.position = camera.transform.position;
     }
 
-    public void Play(MusicLayers layers) {
+    public Coroutine Play(MusicLayers layers) {
+        return StartCoroutine(PlayAsync(layers));
+    }
+
+    private IEnumerator PlayAsync(MusicLayers layers) {
+        Debug.Log("PLAYING!");
         if (_currentLayers != null) {
             Stop();
         }
 
-        for (int i = _audios.Count; i < layers.layers.Length; i++) {
+        _currentLayers = layers.layers;
+
+        for (int i = _audios.Count; i < _currentLayers.Length; i++) {
             _audios.Add(gameObject.AddComponent<AudioSource>());
         }
 
         double time = AudioSettings.dspTime;
         var coros = new List<Coroutine>();
 
-        for (int i = 0; i < layers.layers.Length; i++) {
-            _audios[i].clip = layers.layers[i].clip;
+        for (int i = 0; i < _currentLayers.Length; i++) {
+            _audios[i].clip = _currentLayers[i].clip;
             _audios[i].volume = 0f;
-            if (layers.layers[i].automaticallyEnabled) {
+            _audios[i].loop = true;
+            if (_currentLayers[i].automaticallyEnabled) {
                 _audios[i].PlayScheduled(time);
                 int id = i;
-                coros.Add(this.AutoLerp(0f, layers.layers[id].volume, crossfade, _lerpFn,
+                coros.Add(this.AutoLerp(0f, _currentLayers[id].volume, crossfade, _lerpFn,
                     volume => _audios[id].volume = volume));
             }
 
             if (i != 0) {
                 _audios[i].timeSamples = _audios[0].timeSamples;
             }
+        }
+
+        foreach (var coro in coros) {
+            yield return coro;
         }
     }
 
@@ -74,7 +89,7 @@ public class MusicManager : MonoBehaviour
         _sfxSource.PlayOneShot(sfx, volumeScale);
     }
 
-    public IEnumerator StopAsync() {
+    private IEnumerator StopAsync() {
         var audios = _audios;
         _audios = new();
         var coros = new List<Coroutine>();
@@ -94,20 +109,38 @@ public class MusicManager : MonoBehaviour
     }
 
     public Coroutine Stop() {
+        Debug.Log("STOPPING!");
         return StartCoroutine(StopAsync());
     }
 
     public void EnableLayer(int id) {
-        _audios[id].volume = 0;
-        _audios[id].Play();
-        _audios[id].timeSamples = _audios[0].timeSamples;
-        this.AutoLerp(0f, _currentLayers.layers[id].volume, crossfade, _lerpFn,
-            volume => _audios[id].volume = volume);
+        Debug.Log($"ENABLING LAYER {id} of {_audios.Count}!!");
+
+        var audio = _audios[id];
+
+        audio.volume = 0;
+        audio.Play();
+        audio.timeSamples = _audios[0].timeSamples;
+        this.AutoLerp(0f, _currentLayers[id].volume, crossfade, _lerpFn,
+            volume => audio.volume = volume);
     }
 
     public void DisableLayer(int id) {
-        var coro = this.AutoLerp(_currentLayers.layers[id].volume, 0f, crossfade, _lerpFn,
+        Debug.Log($"DISABLING LAYER {id}!!");
+
+        var coro = this.AutoLerp(_currentLayers[id].volume, 0f, crossfade, _lerpFn,
             volume => _audios[id].volume = volume);
         this.WaitThen(coro, () => { _audios[id].Stop(); });
     }
+
+    private void RecombobulateVolumes() {
+        float volSum = 0f;
+        for (int i = 0; i < _currentLayers.Length; i++) {
+            if (_audios[i].isPlaying) {
+                volSum += _currentLayers[i].volume;
+            }
+        }
+        
+    }
+    
 }
