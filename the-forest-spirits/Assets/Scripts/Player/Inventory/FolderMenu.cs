ce270c6fbf98512fc04344f1ad78e39c242cb3d5
Jonathan Kula <jonathan@jdkula.dev>
2023-06-06 04:21:38 -0700

    but what if SOUNDS

--- the-forest-spirits/Assets/Scripts/Player/Inventory/FolderMenu.cs
+++ the-forest-spirits/Assets/Scripts/Player/Inventory/FolderMenu.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class FolderMenu : AutoMonoBehaviour, IMouseEventReceiver
 {
     // Start is called before the first frame update
-    
+
     public bool IsOpen => animator.GetBool(Open);
-    
+
+    public AudioClip onOpenClip;
+    public AudioClip onCloseClip;
+
     [AutoDefault, ReadOnly]
     public Animator animator;
+
     private static readonly int Open = Animator.StringToHash("Open");
 
 
     public void OpenFolder() {
         KeyValueStore.Instance.Set(KVStoreKey.HasOpenedCaseFile, "true");
         animator.SetBool(Open, true);
+        if (onOpenClip != null) {
+            Lil.Music.PlaySFX(onOpenClip, 0.2f);
+        }
     }
 
     public void CloseFolder() {
         animator.SetBool(Open, false);
+        if (onCloseClip != null) {
+            Lil.Music.PlaySFX(onCloseClip, 0.2f);
+        }
     }
 
     public bool IsMouseInteractableAt(Vector2 screenPos, Camera cam, IMouseAttachable receiver = null) {
