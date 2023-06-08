6110cc57d5eb0c348bcd93cd0091a7d35f940b7c
Jonathan Kula <jonathan@jdkula.dev>
2023-06-08 01:37:52 -0700

    Click people with the appropriate items to continue! instead of just having them

--- the-forest-spirits/Assets/_Features/Dialogue/KeyValueStore/KVStoreKey.cs
+++ the-forest-spirits/Assets/_Features/Dialogue/KeyValueStore/KVStoreKey.cs
@@ -2,11 +2,22 @@ public enum KVStoreKey
 {
     HasAcquiredCaseFile = 1,
     HasOpenedCaseFile = 2,
-    HasSunshined = 3,
+
+    L1ClickedWithHandle = 11,
+    L1HasStencil = 41,
+    L1HasNote = 42,
+    L1DidManifest = 43,
+
+    L2ClickedWithSunshine = 3,
+    L2HasSunshineStencil = 22,
     
-    L4HasStencil = 41,
-    L4HasNote = 42,
-    L4DidManifest = 43,
+    L3HasBridgeStencil = 31,
+
+    L4DidClickWithIce = 51,
+    L4GotFlowerStencil = 52,
+    L4GotFlower = 53,
+    L4GotIceStencil = 54,
+    L4GotIce = 55,
 
     FolderOpen = 990,
     StencilAttached = 991,
